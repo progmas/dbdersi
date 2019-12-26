19eb19ef40262aec0eee251cac2a3672023871ef
Muhammed Ali SÜRÜCÜ <progmas@gmail.com>
2019-12-26 13:14:06 +0300

    sync

--- db/migrate/20191226084823_add_worktype_to_employee.rb
+++ db/migrate/20191226084823_add_worktype_to_employee.rb
@@ -1,5 +1,8 @@
 class AddWorktypeToEmployee < ActiveRecord::Migration[6.0]
   def change
+    #execute  <<-SQL
+    #  CREATE TYPE worktype AS ENUM ('Tam Zamanlı','Yarı Zamanlı','Dönemsel','Proje Bazlı','Stajer','Serbest Zamanlı','Saatlik');
+    #SQL
     add_column :employees, :worktype, :work_type
     add_column :advertisements, :worktype, :worktype
   end
